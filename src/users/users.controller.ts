a6ad33e14b3643b7c6ffe8c08e3ab8003b074980
MiguelGarcia98GitHub <miguelgarciard98@gmail.com>
2023-05-14 11:10:29 +0200

    added HTTPException handlers to CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,7 +14,7 @@ export class UsersController {
   }
 
   @Get(':id')
-  getUser(@Param("id", ParseIntPipe) id: number): Promise<User> {
+  getUser(@Param("id", ParseIntPipe) id: number) {
     return this.usersService.getUser(id);
   }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createUser.dto';
 import { updateUserDto } from './dto/updateUser.dto';
+import {HttpStatus} from '@nestjs/common'
 
 @Injectable()
 export class UsersService {
@@ -17,25 +18,75 @@ export class UsersService {
     return this.userRepository.find()
   }
 
-  getUser(id: number): Promise<User> {
-    return this.userRepository.findOne({
+  async getUser(id: number): Promise<User> {
+    const userFound = await this.userRepository.findOne({
       where: {
         id
       }
     })
+
+    if(!userFound) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    }
+
+    return userFound
   }
 
-  createUser(user: CreateUserDto): Promise<User> {
+  async createUser(user: CreateUserDto): Promise<User> {
+    const userFound = await this.userRepository.findOne({
+    where: {
+      username: user.username
+    }  
+    })
+
+    if (userFound) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST)
+    }
+
     const newUser = this.userRepository.create(user);
     return this.userRepository.save(newUser); // this line of code is async, return will just return a promise here
   }
 
-  deleteUser(id: number) {
-    return this.userRepository.delete({id});
+  async deleteUser(id: number) {
+    // Option 1:
+    // const userFound = await this.userRepository.findOne({
+    //   where: {
+    //     id
+    //   }
+    // });
+
+    // if (!userFound) {
+    //   throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    // }
+
+    // return this.userRepository.delete({id})
+
+
+    // Option 2: (slightly shorter)
+    const result = await this.userRepository.delete({
+      id
+    })
+
+    if (result.affected === 0) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    }
+
+    return result;
   }
 
-  updateUser(id: number, user: updateUserDto) {
-    return this.userRepository.update({id}, user);
+  async updateUser(id: number, user: updateUserDto) {
+   const userFound = await this.userRepository.findOne({
+    where: {
+      id
+    }
+   });
+
+   if (!userFound) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+   }
+
+   const updatedUser = Object.assign(userFound, user);
+   return this.userRepository.save(updatedUser);
   }
 
 
